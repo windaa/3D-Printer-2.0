a02a8ab0f82e3b4ca8595e10a98929500642c982
Windaa <home.winda@gmail.com>
2018-02-12 01:38:57 +0100

    implement ver 2

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var upload = require('express-fileupload');
-var Odoo = require('odoo');
-var Odooxml = require('odoo-xmlrpc');
+		// import package
+		var express = require('express');
+		var app = express();
+		var bodyParser = require('body-parser');
+		var mongoose = require('mongoose');
+		var upload = require('express-fileupload');
+		var Odoo = require('odoo');
+		var Odooxml = require('odoo-xmlrpc');
+		var fs = require('fs');
 
-// Alternative 2
-var fs = require('fs');
-var restler = require('restler');
+		// mongooDB connection
 
-// Alternative 3
-var Client = require('node-rest-client').Client;
-var client = new Client();
-// var args;
-
-
-var vorhanden;
-
-//connect to mongoose
 		var uri = 'mongodb://3DP_INF:3dpinf@ds163701.mlab.com:63701/3dp_inf';
 		mongoose.Promise = global.Promise;
 		mongoose.connect(uri);
@@ -26,346 +17,320 @@ var vorhanden;
 
 		db.on("error", console.error.bind(console, "connection error"));
 		db.once("open", function (callback) {
-		  console.log("connection with database are establish");
+			console.log("connection with database are establish");
 		});
 
-
-
-// Odoo1 Connection
+		// odoo connection
 
 		var odoo = new Odoo({
-		  host: 'localhost',
-		  port: 8069,
-		  database: '3dp',
-		  username: '3dpinformatik@gmail.com',
-		  password: 'raspberry'
+			host: 'localhost',
+			port: 8069,
+			database: '3dp',
+			username: '3dpinformatik@gmail.com',
+			password: 'raspberry'
 		});
 
-// Odoo xmlrpc
+		// odoo xmlrpc
 		var odooX = new Odooxml({
-		    url: 'localhost',
-		    port: 8069,
-		    db: '3dp',
-		    username: '3dpinformatik@gmail.com',
-		    password: 'raspberry'
+			url: 'localhost',
+			port: 8069,
+			db: '3dp',
+			username: '3dpinformatik@gmail.com',
+			password: 'raspberry'
 		});
 
 
+		// basic settings for application
+		app.use(bodyParser.urlencoded({extended:true}));
+		app.use(bodyParser.json());
+		app.use(express.static(__dirname + '/public'));
+		app.use(upload());
+		app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
-app.use(upload());
-app.set('view engine', 'ejs');
+		// initiate server
+		app.listen(3000);
+		console.log('Running on port 3000...');
 
-app.listen(3000);
-console.log('Running on port 3000...');
+		// global variable
+		var embedDataTable = new Array();
+		var embedFilename = "";
+		var embedLength = "";
+		var embedWeight = "";
+		var embedStatus = "";
 
 
-//Update Material Odoo --> Mongodb
+		// update material Odoo --> Mongodb
 		var matNameOdoo;
 		var weightOdoo;
 
-			odoo.connect(function (err) {
-			  if (err) { 
-			  	return console.log(err); 
-			  } 
-			  else {
-					  	console.log('odoo connected')
+		odoo.connect(function (err) {
+			if (err) { 
+				return console.log(err); 
+			} 
+			else {
+				console.log('odoo connected')
 
-					  	var params = {
-										fields: [ 'name','weight', 'volume'],
-										limit: 5,
-										offset: 0,  
-									 }; 
+				var params = {
+					fields: ['name','weight', 'volume'],
+					limit: 5,
+					offset: 0,  
+				}; 
 
-						odoo.browse_by_id('product.product', params, function (err, products) 
-						{
-						  	
-							if (err) { return console.log(err); }
-							 
-							for(var i = 0; i < products.length; i++)
-									{
-										 //console.log(products.length);
-										 matNameOdoo = products[i].name;
-										 weightOdoo = products[i].weight;
-										 console.log(matNameOdoo + ";" + weightOdoo);
 
-										 // Update
-										var myQuery = { materialName: matNameOdoo };
-										var newValues = { $set: {filamentWeight:weightOdoo}};
+				odoo.browse_by_id('product.product', params, function (err, products) 
+				{
 
-										db.collection("current_material").updateOne(myQuery, newValues, function(err, res) {
-											if (err) throw err;
-										}); 
-									}
+					if (err) { return console.log(err); }
 
-						}); 
+
+					for(var i = 0; i < products.length; i++)
+					{
+													 //console.log(products.length);
+													 matNameOdoo = products[i].name;
+													 weightOdoo = products[i].weight;
+													 console.log(matNameOdoo + ";" + weightOdoo);
+
+													 // Update
+													 var myQuery = {materialName:matNameOdoo};
+													 var newValues = { $set: {filamentWeight:weightOdoo}};
+
+													 db.collection("current_material").updateOne(myQuery, newValues, function(err, res) {
+													 	if (err) throw err;
+													 }); 
+													}
+
+
+												}); 
+
+			}
+		});
+
+		app.get('/', function(req, res){
+			res.render('index.ejs');
+		});
+		
+		app.get('/check', function(req, res){
+			db.collection("current_material").find({}).toArray(function(err, data) {
+			if (err) throw err;
+			res.render('material.ejs', {datas:data});
+			});			
+		});
+
+		app.get('/file', function(req, res){
+			res.render('submit.ejs');	
+		});
+
+		/**
+		app.post('/upload', function(req, res){
+			res.render('status.ejs',
+			{ 
+								filename: "embedFilename",
+								lengthEjs: "embedLength",
+								weightEjs: "embedWeight",
+								available : "embedStatus"
+			});	
+		});
+		**/
+
+		app.post('/upload', function(req, res) { 
+			
+			if(req.files) {
+				var file = req.files.filename;
+				var filename = file.name;
+				var fileInString;
+				var filament;
+				var filamentWithoutMeter;
+				var filamentWithoutSpace;
+				var filamentNum;
+				var words;
+				var list = [];
+				var volume;
+				var weight;
+				var mi;
+				var newWeight;
+				
+
+				file.mv('./uploads/' + filename);
+
+				fs.readFile('./uploads/' + filename, 'utf8', function (err,data) {
+					if (err) {
+						return console.log(err);
 					}
-			});
+
+					fileInString = data;
+
+					words = fileInString.split(";");
+
+					
+					for(var i = 0; i < words.length; i++) {
+						var part = [];
+						list[i] = words[i];
+						part = list[i].split(":");
+						if(part[0] == "Filament used") {
+							filament = part[1];
+						}
+					}
+
+					console.log(filament);
+					
+					filamentWithoutMeter = filament.replace("m","");
+					filamentWithoutSpace = filamentWithoutMeter.trim();
+					filamentNum = parseFloat(filamentWithoutSpace);
 
 
+					filamentWithoutMeter = filament.replace("m","");
+					filamentWithoutSpace = filamentWithoutMeter.trim();
+					filamentNum = parseFloat(filamentWithoutSpace);
+					
+					filamentWithoutMeter = filament.replace("m","");
+					filamentWithoutSpace = filamentWithoutMeter.trim();
+
+					filamentNum = parseFloat(filamentWithoutSpace);
 
 
-app.get('/', function(req, res){
-	res.render('index.ejs');
-});
-
-app.get('/submit', function(req, res){
-	res.render('submit.ejs');
-});
-
-// go to home
-app.get('/index', function(req, res, next){
-	res.render('index.ejs', { meldung: ''});
-});
+					// Embed Variable
+					embedFilename = filename;
+					embedLength = filamentWithoutSpace;
+					embedWeight = weight;
 
 
-app.get('/check', function(req, res){
+					mi = req.body.MatID;
+					console.log(JSON.stringify(mi));
 
-	/**
-	db.collection("current_material").findOne({materialID:'1001'}, function(err, data) {
-		console.log(data)
-		res.render('material.ejs',
-			{	matID:data.materialID,	
-				matName:data.materialName,
-				matFarbe:data.materialFarbe,
-				matFil:data.filament
-			});
-	});
-	**/
+					db.collection("current_material").findOne({materialID:mi},function(err, data) {
+						if (err) throw err;
+						console.log(filamentNum);
+						console.log(data.filamentWeight);
 
-	// Test comment
-
-		db.collection("current_material").find({}).toArray(function(err, data) {
-		    if (err) throw err;
-		    console.log(data);
-		    res.render('material.ejs', {datas:data});
-	  	});
-
-	/**
-	var array = [];
-	var cursor = db.collection("current_material").find({});
-	cursor.forEach(function(err, data){
-		array.push(data.materialID);
-		res.render('material.ejs', {datas:array});
-	});
-	**/
-});
+						volume = filamentNum * 3.14 * (data.DuchmesserFilament / 2) * (data.DuchmesserFilament / 2);
+						console.log(volume);
+						weight = volume * data.MaterialDichte
+						console.log(weight);
 
 
-app.get('/help', function(req, res){
-	res.render('help.ejs');
-});
+						if(weight > data.filamentWeight)
+						{
+							embedStatus = "Material is not available"
 
-
-
-app.post('/upload', function(req, res) { 
-	
-	if(req.files) {
-		var file = req.files.filename;
-		var filename = file.name;
-		var fileInString;
-		var filament; //länge des Materials von Auftrag
-		var filamentWithoutMeter;
-		var filamentWithoutSpace;
-		var filamentNum;
-		var words;
-		var list = [];
-		var volume;
-		var weight;
-		var mi;
-		var newWeight;
-		
-
-		file.mv('./uploads/' + filename);
-
-		fs.readFile('./uploads/' + filename, 'utf8', function (err,data) {
-				  if (err) {
-				    return console.log(err);
-				  }
-			  
-			fileInString = data;
-			 //console.log(fileInString);
-
-		  	words = fileInString.split(";");
-
-			
-			for(var i = 0; i < words.length; i++) {
-			    var part = [];
-			    list[i] = words[i];
-				part = list[i].split(":");
-			 	if(part[0] == "Filament used") {
-			        filament = part[1];
-			    }
-			}
-
-			console.log(filament);
-			
-			filamentWithoutMeter = filament.replace("m","");
-			filamentWithoutSpace = filamentWithoutMeter.trim();
-			filamentNum = parseFloat(filamentWithoutSpace);
-
-
-			filamentWithoutMeter = filament.replace("m","");
-			filamentWithoutSpace = filamentWithoutMeter.trim();
-			filamentNum = parseFloat(filamentWithoutSpace);
-			
-			filamentWithoutMeter = filament.replace("m","");
-			filamentWithoutSpace = filamentWithoutMeter.trim();
-		
-			filamentNum = parseFloat(filamentWithoutSpace);
-
-
-			mi = req.body.MatID;
-			console.log(JSON.stringify(mi));
-
-			db.collection("current_material").findOne({materialID:mi},function(err, data) {
-			    if (err) throw err;
-			    console.log(filamentNum);
-			    console.log(data.filamentWeight);
-
-			    volume = filamentNum * 3.14 * (data.DuchmesserFilament / 2) * (data.DuchmesserFilament / 2);
-			    console.log(volume);
-			    weight = volume * data.MaterialDichte
-			    console.log(weight);
-
-
-			   		if(weight > data.filamentWeight)
-			   			{
-				   			console.log("unavailable");
-				   			res.render('uploadInfo.ejs',
+							console.log("unavailable");
+							res.render('status.ejs',
 							{ 
-							filename: filename,
-							lengthEjs: filamentWithoutSpace,
-							weightEjs: weight,
-							available : "Material is not available"
+								filename: embedFilename,
+								lengthEjs: embedLength,
+								weightEjs: embedWeight,
+								available : embedStatus
 							});
-				   		} 
-				   		else 
-				   		{
-							console.log("available");
-							var myobj = { materialID: mi, lange: filamentWithoutSpace, filamentWeight: weight };
-							
-							// Insert to request_material
-							db.collection("request_material").insertOne(myobj, function(err, res) {
-								    if (err) throw err;
-								    console.log("1 document inserted");
-								 });
+						} 
+						else 
+						{
 
-							// Update
-							newWeight = (data.filamentWeight - weight);
-							var myparam = { materialID: mi };
-							var newValue = { $set: {filamentWeight:newWeight}};
+						   			// upload gcode file to octoprint
+						   			// unwrappeduploadToOctoprint(filename);
 
-							db.collection("current_material").updateOne(myparam, newValue, function(err, res) {
-								    if (err) throw err;
-								    console.log("1 document updated");
-								}); 
+						   			embedStatus = "Material is available";
 
-							console.log(mi);
-							console.log(newWeight);
+						   			console.log("available");
+						   			var myobj = { materialID: mi, lange: embedLength, filamentWeight: embedWeight };
 
-							// Render
-					   		res.render('uploadInfo.ejs',
-							{ 
-								filename: filename,
-								lengthEjs: filamentWithoutSpace,
-								weightEjs: weight,
-								available : "Material is available"
+									// insert to request_material
+									db.collection("request_material").insertOne(myobj, function(err, res) {
+										if (err) throw err;
+										console.log("1 document inserted");
+									});
 
+									// update
+									newWeight = (data.filamentWeight - weight);
+									var myparam = { materialID: mi };
+									var newValue = { $set: {filamentWeight:newWeight}};
+
+									db.collection("current_material").updateOne(myparam, newValue, function(err, res) {
+										if (err) throw err;
+										console.log("1 document updated");
+									}); 
+
+									console.log(mi);
+									console.log(newWeight);
+
+									// render
+									res.render('status.ejs',
+									{ 
+										filename: embedFilename,
+										lengthEjs: embedLength,
+										weightEjs: embedWeight,
+										available : embedStatus
+									});
+								}
 							});
-			   			}
-		  	});
-		});
+				});
 
-					odooX.connect(function (err) {
-					    if (err) { return console.log(err); }
-					    console.log('Connected to Odoox server.');
+							// update Odoo
+							odooX.connect(function (err) {
+								if (err) { return console.log(err); }
+								console.log('Connected to Odoox server.');
 
-					    var id = parseFloat(mi);
-					    var vol= (newWeight/1.24);
+								var id = parseFloat(mi);
+								var vol= (newWeight/1.24);
 
-					    var inParams = [];
-					    inParams.push([id]); //id to update
-					    inParams.push({'weight': newWeight},{'volume': vol});
-					    console.log(mi);
-					    console.log(newWeight);
-					    console.log(vol);
-					    var params = [];
-					    params.push(inParams);
-					    odooX.execute_kw('product.product', 'write', params, function (err, products) {
-						        if (err) { return console.log(err); }
-						        console.log('Result: ', products);
-					    	});
+								var inParams = [];
+							    inParams.push([id]); //id to update
+							    inParams.push({'weight': newWeight},{'volume': vol});
+							    console.log(mi);
+							    console.log(newWeight);
+							    console.log(vol);
+							    var params = [];
+							    params.push(inParams);
+							    odooX.execute_kw('product.product', 'write', params, function (err, products) {
+							    	if (err) { return console.log(err); }
+							    	console.log('Result: ', products);
+							    });
+							});
+
+							odooX.connect(function (err) {
+								if (err) { return console.log(err); }
+								console.log('Connected to Odoox server.');
+
+								var id2 = parseFloat(mi);
+								var vol= (newWeight/1.24);
+
+								var inParams2 = [];
+							    inParams2.push([id2]); //id to update
+							    inParams2.push({'volume': vol});
+							    var params2 = [];
+							    params2.push(inParams2);
+							    odooX.execute_kw('product.product', 'write', params2, function (err, products) {
+							    	if (err) { return console.log(err); }
+							    	console.log('Result: ', products);
+							    });
+							});
+
+						}
 					});
 
-					odooX.connect(function (err) {
-					    if (err) { return console.log(err); }
-					    console.log('Connected to Odoox server.');
+		unwrappeduploadToOctoprint = function(x){
+			FormData = require('form-data'),
+			fs = require('fs') ;
+			var request = require("request");
 
-					    var id2 = parseFloat(mi);
-					    var vol= (newWeight/1.24);
+			var form ={
+				file: {
+					value: fs.createReadStream('uploads/'+ x),
+					options: {filename: x}
+				}
+			};
 
-					    var inParams2 = [];
-					    inParams2.push([id2]); //id to update
-					    inParams2.push({'volume': vol});
-					    var params2 = [];
-					    params2.push(inParams2);
-					    odooX.execute_kw('product.product', 'write', params2, function (err, products) {
-						        if (err) { return console.log(err); }
-						        console.log('Result: ', products);
-						    });
-					});
-
-	}
-});
-
-
-
-
-// Alternative 1
-		/** 
-		const data = document.getElementById('pushData');
-			data.addEventListener('submit', (e) => {
-			e.preventDefault()
-			const formData = new formData(data)
-			fetch('192.168.137.20/api/files/local', {
-			method: 'POST',
-			body: formData,
-			headers: {
-				'x-api-key' : '4E2F97268A004C30BFC84FD9D9070C47'
-			}
-		})
-		})	
-		**/
-
-		// Alternative 2
-		/**
-		fs.stat(filename, function(err, stats) {
-    	restler.post("http://192.168.137.20/api/files/local", {
-        multipart: true,
-        headers:{
-        	'x-api-key' : '4E2F97268A004C30BFC84FD9D9070C47'
-        },
-    	file: req.files.filenames
-    	}).on("complete", function(data) {
-        console.log(data);
-    	});
-		});
-		**/
-		
-
-		// Alternative 3
-		/**
-		var args = {
-			data: file,
-			headers: {
-				'x-api-key' : '4E2F97268A004C30BFC84FD9D9070C47'
-			}
+			var options = {
+				method: 'POST',
+		    url: 'http://192.168.137.240/api/files/local', // need to modify if the ip changed
+		    headers: { 'x-api-key': '4E2F97268A004C30BFC84FD9D9070C47'},
+		    formData: form
 		};
 
-		client.post('http://192.168.137.20/api/files/local', args, function (data, response) {
-    	console.log(req.files);
+		var req = request(options, function (error, response, body) {
+			if (error) throw new Error(error);
+			console.log(body);
 		});
-		**/
+	};
+
+
+
+
